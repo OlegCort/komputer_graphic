b9ee22f8083cf399d08900e112071a4b30062af6
Andriy-Symon <drinlviv@gmail.com>
2023-12-02 19:56:22 +0200

    sdfdsf

--- movementScript.js
+++ movementScript.js
@@ -14,12 +14,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
    
     function drawTriangle() {
-        var firstVertexX = document.getElementById('firstVertexX').value;
-        
-        var firstVertexY = document.getElementById('firstVertexY').value;
-        var secondVertexX = document.getElementById('secondVertexX').value;
-        var secondVertexY = document.getElementById('secondVertexY').value;
-        var heightInput = document.getElementById('trianglesHeight').value;
+        var firstVertexX = parseInt(document.getElementById('firstVertexX').value, 10);
+        var firstVertexY = parseInt(document.getElementById('firstVertexY').value, 10);
+        var secondVertexX = parseInt(document.getElementById('secondVertexX').value, 10);
+        var secondVertexY = parseInt(document.getElementById('secondVertexY').value, 10);
+        var heightInput = parseInt(document.getElementById('trianglesHeight').value, 10);
 
         console.log(firstVertexX);
         console.log(firstVertexY);
@@ -32,45 +31,36 @@ document.addEventListener("DOMContentLoaded", function () {
         drawIsoscelesTriangle(firstVertexX, firstVertexY, secondVertexX, secondVertexY, heightInput);
     }
 
+
     function drawIsoscelesTriangle(baseApex1X, baseApex1Y, baseApex2X, baseApex2Y, height) {
-
-        // Clear previous drawings
-        //ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-        // Draw the base
-        ctx.beginPath();
-        ctx.moveTo(baseApex1X, baseApex1Y);
-        ctx.lineTo(baseApex2X, baseApex2Y);
-        ctx.stroke();
-
-        // Calculate the midpoint of the base
-        var midBaseX = (baseApex1X + baseApex2X) / 2;
-        var midBaseY = (baseApex1Y + baseApex2Y) / 2;
-
-        // Calculate the direction vector of the base
-        var dirX = baseApex2X - baseApex1X;
-        var dirY = baseApex2Y - baseApex1Y;
-
-        // Calculate the length of the base
-        var baseLength = Math.sqrt(dirX * dirX + dirY * dirY);
-
-        // Calculate unit vector perpendicular to the base
-        var perpX = -dirY / baseLength;
-        var perpY = dirX / baseLength;
-
-        // Calculate the third apex using the height and the perpendicular direction
-        var apexX = midBaseX + perpX * height;
-        var apexY = midBaseY + perpY * height;
-
-        // Draw the remaining sides
-        ctx.moveTo(baseApex1X, baseApex1Y);
-        ctx.lineTo(apexX, apexY);
-        ctx.lineTo(baseApex2X, baseApex2Y);
-        ctx.closePath();
-
-        ctx.lineWidth = 2;
-        ctx.strokeStyle = 'black';
-        ctx.stroke();
+        const canvas = document.getElementById("movementCanvas");
+        const ctx = canvas.getContext('2d');
+        if (canvas.getContext) {
+            // Calculate the midpoint of the base
+            var midX = (baseApex1X + baseApex2X) / 2;
+            var midY = (baseApex1Y + baseApex2Y) / 2;
+    
+            // Calculate the top apex coordinates
+            var dx = baseApex2X - baseApex1X;
+            var dy = baseApex2Y - baseApex1Y;
+            var distBase = Math.sqrt(dx * dx + dy * dy);
+            var angle = Math.atan2(dy, dx);
+    
+            var topApexX = midX - height * Math.sin(angle);
+            var topApexY = midY + height * Math.cos(angle);
+    
+            // Draw the triangle
+            ctx.beginPath();
+            ctx.moveTo(baseApex1X, baseApex1Y);
+            ctx.lineTo(baseApex2X, baseApex2Y);
+            ctx.lineTo(topApexX, topApexY);
+            ctx.closePath();
+    
+            // Style the triangle
+            ctx.lineWidth = 2;
+            ctx.strokeStyle = '#000000';
+            ctx.stroke();
+        }
     }
 
     function drawAxis() {
@@ -87,7 +77,7 @@ document.addEventListener("DOMContentLoaded", function () {
     
         ctx.strokeStyle = 'black';
         ctx.stroke();
-        ctx.closePath();
+        //ctx.closePath();
     }
     
     
